074afd7e79f37612fa2d1193234fb772375b768b
Gabriel-Malenowitch <gabrielbotelhomalenowitch@gmail.com>
2023-01-02 08:42:01 -0300

    feat: improve errors log

--- test.mjs
+++ test.mjs
@@ -1,22 +1,25 @@
 const path = ''
 const commands = []
 
+// Array for keep errors and show after test
 const errors = []
 
+// Test each command
 commands.forEach(command => {
     try{
         $`${path} ${command}`
     } catch (error) {
         console.log(error)
-        errors.push(command)
+        errors.push({ command, err: error })
     }
 })
 
+// If we have errors, show them
 if(errors.length) {
     $`=================================================`
     $`echo We have problems to test some commands:`
     errors.forEach(error => {
-        console.log(`- ${error}`)
+        console.log(`- Command "${error.command}" get: ${error.err}`)
     })
     $`=================================================`
 }
